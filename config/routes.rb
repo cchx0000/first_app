200d757d8f44a3e015e5c614236a27deb121e3be
Chenxi Cai <Chenxi_Cai@live.com>
2015-04-07 17:56:35 -0400

    dd

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   resources :users
   root 'welcome#index'
 
-  get "users"
-  get "microposts"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
